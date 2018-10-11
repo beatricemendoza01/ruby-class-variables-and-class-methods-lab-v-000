761efac383837191924246bdcd8414a591cb8c54
Beatrice Mendoza <mendoza@nyu.edu>
2018-10-11 01:47:34 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,19 @@
 class Song
+  
+  @@count = 0
+  @@genres = []
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre unless genres.include?(genre)
+  end
+  
+  attr_accessor :name, :artist, :genre
+  
+  def self.count
+    @@count
+  end
 end
